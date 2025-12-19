ad7fdc52f38554dda8b67c027eee6d4a22af62c0
Mohamad Ardebili <mohammadardebili123987@gmail.com>
2025-12-19 17:32:56 +0330

    add profile-model

--- base/models/_accounts.py
+++ base/models/_accounts.py
@@ -1,8 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
 
 class UserManager(BaseUserManager):
     """custom user manager"""
+
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("is_staff", True)
@@ -14,14 +16,14 @@ class UserManager(BaseUserManager):
             raise ValueError("is_staff value is not valid")
         if not extra_fields.get("is_superuser"):
             raise ValueError("is_superuser value is not valid")
-        
+
         return self.create_user(email=email, password=password, **extra_fields)
 
     def create_user(self, email, password, **extra_fields):
         extra_fields.setdefault("is_active", True)
         if not extra_fields.get("is_active"):
             raise ValueError("is_active value is not valid")
-        
+
         try:
             user = self.model(email=email, **extra_fields)
             user.set_password(password)
@@ -30,7 +32,6 @@ class UserManager(BaseUserManager):
         except Exception as e:
             print(e)
             raise ValueError(e)
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -54,6 +55,27 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def __str__(self):
         return f"User Object: {self.id} - {self.get_full_name()}"
+
+
+class Profile(models.Model):
+    """ store additional data for each user """
+    # numeric fields
+    age = models.PositiveSmallIntegerField()
+    weight = models.FloatField()
+    height = models.FloatField()
+    BMI = models.FloatField()
+
+    # image-related fields
+    avatar = models.ImageField(upload_to="user_avatar/")
+
+    # text-related fields
+    description = models.TextField(blank=True, null=True)
+
+    # relations
+    user = models.OneToOneField("base.User", on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return f"Profile Object: {self.id} - {self.user.get_full_name()}"
